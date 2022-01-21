611070987947f92a3e67cb3d3b80442419d6b53b
ViririTT <timothyviriri12@gmail.com>
2022-01-21 17:02:06 +0200

    new code for adding rows to database

--- index.js
+++ index.js
@@ -2,46 +2,50 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
-var dbo = null;
+const db_connection = require("./mysql_db");
 app.use(express.json());
-
-MongoClient.connect(url, function (err, db) {
-  if (err) throw err;
-  dbo = db.db("matelico");
-});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
+
 app.get("/panelbeat", (req, res) => {
   res.sendFile(path.join(__dirname + "/panelbeat.html"));
 });
+
 app.get("/carwash", (req, res) => {
   res.sendFile(path.join(__dirname + "/carwash.html"));
 });
+
 app.post("/recordPanelBeaterData", (req, res) => {
   var myobj = req.body;
   myobj.dateIn = new Date().getTime();
 
-  dbo
-    .collection("PanelBeatersCustomers")
-    .insertOne(myobj, function (err, result) {
-      if (err) throw err;
+  console.log(myobj);
+
+  var sql =
+    "INSERT INTO  metalico.panelbeaters (DateIn, DateOut,Client,Insurance,ClaimNo,InvoiceNo,Vehicle,Reg,SaleAmount,ExcessAmount,SettlementAmount,ExcessDatePaid,FRCdate,InvoiceDate,Paid,Deposit,Method,Comments,Status) VALUES (DateIn, DateOut,Client,Insurance,ClaimNo,InvoiceNo,Vehicle,Reg,SaleAmount,ExcessAmount,SettlementAmount,ExcessDatePaid,FRCdate,InvoiceDate,Paid,Deposit,Method,Comments,Status)";
+
+  db_connection.query(sql, function (err, result) {
+    if (err) {
+      console.error(err);
+      res.send("erro");
+    } else {
+      console.log("1 record inserted");
       res.send("success");
-    });
-});
-
-app.post("/recordCarwashCustomers", (req, res) => {
-  var myobj = req.body;
-  myobj.dateIn = new Date().getTime();
-
-  dbo.collection("carWashCustomers").insertOne(myobj, function (err, result) {
-    if (err) throw err;
-    res.send("success");
+    }
   });
 });
+
+// app.post("/recordCarwashCustomers", (req, res) => {
+//   var myobj = req.body;
+//   myobj.dateIn = new Date().getTime();
+
+//   dbo.collection("carWashCustomers").insertOne(myobj, function (err, result) {
+//     if (err) throw err;
+//     res.send("success");
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ mysql_db.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  insecureAuth: true,
+  database: "metalico",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = connection;
