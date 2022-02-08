0ada80f0bcea1f96ec1dadad66d534f9e19ac69e
ViririTT <timothyviriri12@gmail.com>
2022-02-08 11:32:12 +0200

    moved matelico

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8082;
+// app.listen(port, () => console.log(`Listening on port ${port}..`));
 const path = require("path");
 const db_connection = require("./mysql_db");
 const moment = require("moment");
