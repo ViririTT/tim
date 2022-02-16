a107c7044303588135f57c387db44bbaab55c02e
ViririTT <timothyviriri12@gmail.com>
2022-02-16 09:12:00 +0200

    db update

--- stackmysql.sql
+++ stackmysql.sql
@@ -5,3 +5,7 @@ ALTER TABLE `panelbeaters` CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREME
 SHOW COLUMNS FROM panelbeaters;
 ALTER TABLE `panelbeaters`  ADD `id` INT NOT NULL AUTO_INCREMENT ,  ADD   PRIMARY KEY  (`id`);
 ALTER TABLE `panelbeaters` DROP `id`;
+GRANT ALL ON *.* to 'tim'@'localhost' identified by 'passw0rd';
+
+FLUSH PRIVILEGES;
+20.87.32.246/:10060
