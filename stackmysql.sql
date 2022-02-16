e1431bf93cf7edbb6fe19ee660c56545112d1f6e
ViririTT <timothyviriri12@gmail.com>
2022-02-16 08:26:04 +0200

    change title to metalico motors

---
+++ stackmysql.sql
@@ -0,0 +1,7 @@
+ALTER TABLE panelbeaters DROP IF EXISTS my_column;
+ALTER TABLE panelbeaters MODIFY id int NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `panelbeaters` CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT ;
+SHOW COLUMNS FROM panelbeaters;
+ALTER TABLE `panelbeaters`  ADD `id` INT NOT NULL AUTO_INCREMENT ,  ADD   PRIMARY KEY  (`id`);
+ALTER TABLE `panelbeaters` DROP `id`;
