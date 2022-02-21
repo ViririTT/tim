2b09c38a1b99522bc1aca070dca80b8cf35c61c9
ViririTT <timothyviriri12@gmail.com>
2022-02-21 15:16:01 +0200

    update

--- mysql_db.js
+++ mysql_db.js
@@ -2,8 +2,8 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "password",
+  user: "tim",
+  password: "passw0rd",
   insecureAuth: true,
   database: "metalico",
 });
