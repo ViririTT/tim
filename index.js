ac7b3860acf79be0a00769439fea534d3121472f
ViririTT <timothyviriri12@gmail.com>
2022-04-11 10:02:37 +0200

    fall back2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8082;
+const port = 3000;
 //  process.env.PORT ||
 // app.listen(port, () => console.log(`Listening on port ${port}..`));
 const path = require("path");
